de2178072c21ec6f1b0a55dc4debb8a0ff0450f4
허세훈 <tpgns0539@naver.com>
2019-05-04 17:26:28 +0900

    add "findSummoner() method"

--- src/main/java/org/cnu/realcoding/lolcrawler/repository/SummonerRepository.java
+++ src/main/java/org/cnu/realcoding/lolcrawler/repository/SummonerRepository.java
@@ -3,6 +3,8 @@ package org.cnu.realcoding.lolcrawler.repository;
 import org.cnu.realcoding.lolcrawler.domain.Summoner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -14,6 +16,15 @@ public class SummonerRepository {
     //MongoDB에 Summoner를 삽입하는 메소드 추가
     public Summoner insertSummoner(Summoner summoner) {
         return mongoTemplate.insert(summoner);
+    }
+
+    //Summoner의 이름을 가지고 DB에서 해당 Summoner을 찾아 반환해주는 메소드 추가
+    public Summoner findSummoner(String name) {
+        Query query = new Query();
+
+        query.addCriteria(Criteria.where("name").is(name));
+
+        return mongoTemplate.findOne(query, Summoner.class);
     }
 
     //Update 메소드 추가할것!!
