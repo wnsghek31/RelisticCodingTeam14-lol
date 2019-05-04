694f2326a119dd98e8d39f6be3f432da3c524498
허세훈 <tpgns0539@naver.com>
2019-05-04 16:15:24 +0900

    add "Repository"

---
+++ src/main/java/org/cnu/realcoding/lolcrawler/repository/SummonerRepository.java
@@ -0,0 +1,19 @@
+package org.cnu.realcoding.lolcrawler.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Repository
+public class SummonerRepository {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    //MongoDB에 Summoner를 삽입하는 메소드 추가
+    public Summoner insertSummoner(Summoner summoner) {
+        return mongoTemplate.insert(summoner);
+    }
+
+    //Update 메소드 추가할것!!
+}
